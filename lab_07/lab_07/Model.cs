315c789b87d1ef175f546a612b2ffd368337ceea
Mateusz Malec <mateusz.malec@microsoft.wsei.edu.pl>
2022-06-13 22:07:13 +0200

    feat: add CRUD actions

---
+++ lab_07/lab_07/Model.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace lab_07
+{
+
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<Role> Roles { get; set; }
+
+
+
+        public string ConnectionString { get; }
+
+        public BloggingContext(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(this.ConnectionString);
+        }
+    }
+
+    public class Blog
+    {
+        public long Id { get; set; }
+        public string Url { get; set; }
+
+        public List<Post> Posts { get; } = new();
+    }
+
+    public class Post
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public long BlogId { get; set; }
+        public Blog Blog { get; set; }
+    }
+
+    public class User
+    {
+        public long Id { get; set; }    
+        public string Nickname { get; set; }
+        public long RoleId { get; set; }
+        public List<Task> Tasks { get; } = new();
+    }
+
+    public class Role
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Task
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public long UserId { get; set; }
+    }
+}
---
+++ lab_07/lab_07/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+
+namespace lab_07
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+            string connectionString = @"Data Source=DESKTOP-SO4MQ1P;Initial Catalog=blogdb;Integrated Security=True";
+
+            using (BloggingContext db = new BloggingContext(connectionString))
+            {
+                Console.WriteLine($"Database ConnectionString: {db.ConnectionString}.");
+
+                //////////////////////////////////////////////////////////////////////////////////////////
+                //Console.WriteLine("Inserting data"); // CREATE
+
+                //CREATE Roles
+                //db.Add(new Role { Name = "Admin" });
+                //db.Add(new Role { Name = "Basic" });
+
+                //CREATE Users
+                //db.Add(new User { Nickname = "MaciekAdmin", RoleId = 1 });
+                //db.Add(new User { Nickname = "JanekBasic", RoleId = 2 });
+
+                //CREATE Tasks
+                //db.Add(new Task { Title = "Pranie", Description = "Zrobić zaległe pranie", UserId = 1 });
+                //db.Add(new Task { Title = "Sprzątanie", Description = "Wysprzątać dom", UserId = 2 });
+                //db.Add(new Task { Title = "Zadanie domowe", Description = "Odrobić zadanie domowe", UserId = 2 });
+                //db.SaveChanges();
+
+                //////////////////////////////////////////////////////////////////////////////////////////
+                Console.WriteLine("Querying for a User tasks"); // READ
+
+                User JanekBasic = db.Users.Find((long)2);
+
+                db.Tasks.Where<Task>(task => task.UserId == JanekBasic.Id).ToList<Task>();
+
+                foreach (var task in JanekBasic.Tasks)
+                    Console.WriteLine(task.Title);
+
+                Console.WriteLine("\n\n");
+
+                //////////////////////////////////////////////////////////////////////////////////////////
+                Console.WriteLine("Updating role name"); // UPDATE
+
+                foreach (var role in db.Roles)
+                     Console.Write(role.Name + "  ");
+
+                db.Roles.Find((long)1).Name = "Super Admin";
+                db.SaveChanges();
+
+                Console.WriteLine("\n");
+                foreach (var role in db.Roles)
+                    Console.Write(role.Name + "  ");
+
+                Console.WriteLine("\n\n");
+
+                //////////////////////////////////////////////////////////////////////////////////////////
+                Console.WriteLine("Deleting Task \n"); // DELETE
+                foreach (var task in db.Tasks)
+                    Console.Write(task.Title + " ");
+
+                db.Remove(db.Tasks.Find((long)2));
+                db.SaveChanges();
+
+                Console.WriteLine();
+
+                foreach (var task in db.Tasks)
+                    Console.Write(task.Title + " ");
+
+
+
+
+
+            }
+        }
+    }
+}
